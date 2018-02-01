fed0c6d07b59b00f886c36d7d62a105463c09478
Grant Willford <gwillford@win2.cs.ksu.edu>
2018-02-01 12:34:42 -0600

    commit

--- public/server.js
+++ public/server.js
@@ -9,18 +9,68 @@ const http = require('http');
 //Import the fs library
 const fs=require('fs');
 
-function handleRequest(req, res){
+const cache = {};
+cache['openhouse.html'] = fs.readFileSync('./openhouse.html');
+cache['openhouse.css'] = fs.readFileSync('./openhouse.css');
+cache['openhouse.js'] = fs.readFileSync('./openhouse.js');
 
+function serveIndex(path, res){
+    fs.readdir(path, function(err, files) {
+        if (err) {
+            console.error(err);
+            res.statusCode = 500;
+            res.end("Server Error");
+        }
+        var html = "<p>Index of " + path + "</p>";
+        html += "<ul>";
+        html += files.map(function(item){
+            return "<li><a href ='" + item + "'>" + item + "</a></li>";
+        }).join("");
+        html += "</ul>";
+        res.end(html);
+    });
+}
+
+/** @function serveFile
+ * Serves the specified file with the provided response object.
+ * @param {string} path - specifies the file path to read
+ * @param {http.serverResponse} res - the http response object.
+ */ 
+function serveFile(path, res)
+{
+    fs.readFile(path, function(err, data) {
+        if (err){
+            console.error(err);
+            res.statusCode = 500;
+            res.end("Server Error: Could not read file");
+            return;
+        }
+        res.end(data);
+    });
+}
+
+/**@function handleRequest
+ * Request handler for our http server.
+ * @param {http.ClientRequest} req - the http request object
+ * @param {http.ServerResponse} res - the http response object.
+ */
+function handleRequest(req, res){
+    //Map request urls
     switch(req.url){
         case '/':
+            serveIndex('./',res);
+            break;
         case 'openhouse.html':
-            res.end(fs.readFileSync('./openhouse.html'));
-        break;
+            //res.end(cache['openhouse.html']);
+            serveFile('./openhouse.html',res);
+            break;
         case '/openhouse.css':
-            res.end(fs.readFileSync('./openhouse.css'));
+        //res.end(cache['openhouse.html']);
+            serveFile('./openhouse.css',res);
             break;
         case '/openhouse.js':
-            res.end(fs.readFileSync('./openhouse.js'));
+        //res.end(cache['openhouse.html']);
+            serveFile('./openhouse.js',res);
             break;
         default:
             res.statusCode = 404;
@@ -31,7 +81,7 @@ function handleRequest(req, res){
 //Create the web server
 var server = http.createServer(handleRequest);
 
-//Start listening on port 80
+//Start listening on port PORT
 server.listen(PORT, function() {
-    console.log("Listening on port 1956");
+    console.log("Listening on port " + PORT);
 });
